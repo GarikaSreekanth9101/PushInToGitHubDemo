7df13239afd336abacd25414f765456472650601
Garika Sreekanth <sreekanthgarika91@gmail.com>
2020-12-10 13:20:23 +0530

    Initial Commit

---
+++ PushInToGitHubDemo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PushInToGitHubDemo
+//
+//  Created by Garika Sreekanth on 10/12/20.
+//  Copyright © 2020 Garika Sreekanth. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
